f548b4adfb924082ff584b3b92ad6bf588f08783
khourysamir <samir.khoury1998@gmail.com>
2022-04-28 15:34:20 -0700

    checkpoint 4

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,5 +5,8 @@ class BlogController < ApplicationController
     #     def blog_list
     # render html:"Here is a list of blogs and posts"
     #  end
+        def show
+            @blogs = Blog.find(params[:id])
+        end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'blog' => 'blog#index', as: 'blog'
+  # get 'blog_post/:id' => 'blog#show', as 'blog_post'
   root 'blog#index'
-
 end
